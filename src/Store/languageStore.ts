e708c7e602f5a86dde2cd42f94a94518af6e6bb6
Vinsento <hummanist@gmail.com>
2025-12-31 01:06:21 +0200

    update language save

--- src/Store/languageStore.ts
+++ src/Store/languageStore.ts
@@ -3,6 +3,12 @@ import { isWindow } from "../Utilities/Window"
 import { languageReducer } from "../reducers/languageReducer"
 import { createStore } from "./store"
 
-const initialState = isWindow ? {value: window.navigator.language.slice(0, 2)} : {value: ENGLISH}
+const storedLang =
+  localStorage.getItem('lang') ??
+  (isWindow ? window.navigator.language.slice(0, 2) : ENGLISH)
+
+const initialState = {
+  value: storedLang
+}
 
 export const languageStore = createStore(languageReducer, initialState)
--- src/reducers/languageReducer.ts
+++ src/reducers/languageReducer.ts
@@ -4,10 +4,13 @@ import { RUSSIAN, UKRAINIAN, ENGLISH } from "../Services/Constants"
 export function languageReducer(state: TState<string>, action: TAction) {
   switch (action.type) {
     case ENGLISH:
+      localStorage.setItem('lang', 'en')
       return { value: 'en' }
     case RUSSIAN:
+      localStorage.setItem('lang', 'ru')
       return { value: 'ru' }
     case UKRAINIAN:
+      localStorage.setItem('lang', 'uk')
       return{ value: 'uk' }
     default:
       return state
